17241c8b01c410859935af552f67fe9dc47528bb
Pressx278 <212777533+alexgameregy@users.noreply.github.com>
2025-06-29 18:39:24 +0200

    Initial commit

--- js/index.js
+++ js/index.js
@@ -130,7 +130,7 @@ function CheckFW() {
       });
     }
 
-    document.title = "دوس اكس لتفعيل سوفت | " + fwVersion
+    document.title = "اليكس جيمر لتفعيل سوفت | " + fwVersion
   } else {
     let platform = 'Unknown platform';
 
